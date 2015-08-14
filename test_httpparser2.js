f66781bc843344d3eaad659239171cf64306a24d
1000copy <1000copy@gmail.com>
2015-08-14 22:47:30 +0800

    where put this.push to readable stream ?

--- test_httpparser2.js
+++ test_httpparser2.js
@@ -1,21 +1,18 @@
-var assert = require('assert');
+var Readable = require('stream').Readable;
+var util = require('util');
+
 var HTTPParser = require('./http_parser').HTTPParser
-var REQUEST = HTTPParser.REQUEST;
-var methods = HTTPParser.methods;
 
-var kOnHeaders = HTTPParser.kOnHeaders | 0;
-var kOnHeadersComplete = HTTPParser.kOnHeadersComplete | 0;
-var kOnBody = HTTPParser.kOnBody | 0;
-var kOnMessageComplete = HTTPParser.kOnMessageComplete | 0;
 
-var im = IncomingMessage.prototype 
+
 function IncomingMessage() {
+  Readable.call(this);
   this.parser = new HTTPParser(HTTPParser.REQUEST); 
   var parser = this.parser 
-  parser[kOnHeaders] = this.onHeaders
-  parser[kOnHeadersComplete] = this.onheadercomplete
-  parser[kOnBody] = this.onbody 
-  parser[kOnMessageComplete] = this.oncompleted   
+  parser[HTTPParser.kOnHeaders] = this.onHeaders
+  parser[HTTPParser.kOnHeadersComplete] = this.onheadercomplete
+  parser[HTTPParser.kOnBody] = this.onbody 
+  parser[HTTPParser.kOnMessageComplete] = this.oncompleted   
   // property
   this.headers = [];//: [ 'k1', 'v1' ],
   this.upgrade = false//: false,
@@ -24,14 +21,18 @@ function IncomingMessage() {
   this.versionMajor= 1//: 1,
   this.versionMinor=1 //: 1,
   this.shouldKeepAlive = false//: true 
+  var self = this 
 }
-im.onHeaders =  function(headers, url) {
+
+util.inherits(IncomingMessage, Readable);
+// var im = IncomingMessage.prototype 
+IncomingMessage.prototype.onHeaders =  function(headers, url) {
     // parser.headers = parser.headers.concat(headers);
     // parser.url += url;
     console.log("onHeaders",headers)
 };
 
-im.onheadercomplete = function(info) {
+IncomingMessage.prototype.onheadercomplete = function(info) {
   this.headers = info.headers
   this.upgrade = info.upgrade
   this.method = info.method = HTTPParser.methods[info.method]
@@ -42,25 +43,44 @@ im.onheadercomplete = function(info) {
   // console.log("kOnHeadersComplete",info)
 };
 
-im.onbody = function(b, start, len) {
+IncomingMessage.prototype.onbody = function(b, start, len) {
   // this.emit("chunk",b.slice(start,len))
-  console.log("BODY:")
+  // console.log("BODY:")
   var s = b.slice(start,start+len)
+  // 在何处push？1
+  if (!this.buffer)
+    {this.buffer = []}
+  this.buffer.push(s)
+  // console.log(this.buffer.length,this.buffer)
+  // this.push(s)
   // console.log(b.toString())
-  console.log(s.toString())
+  // console.log(s.toString())
+
   // console.log(start,len)
 
 };
-
-im.oncompleted = function() {
+IncomingMessage.prototype._read = function(){
+  // this.
+  // 在何处push？2
+  console.log("read___",this.buffer)
+  while(this.buffer && (this.buffer.length >0))
+    if (this.push(this.buffer.shift())==false)
+      break;
+  // if (this.buffer && this.buffer.length==0)
+  //   this.push(null)
+}
+IncomingMessage.prototype.oncompleted = function() {
     console.log("oncompleted")
+    // this.push(null)
+    // this.push(null)
 };
-im.execute = function(buffer) {
+IncomingMessage.prototype.execute = function(buffer) {
   this.parser.execute(buffer,0,buffer.length)
 };
 
 
 var CRLF = '\r\n';
+var assert = require('assert');
 
 
 (function() {
@@ -76,8 +96,16 @@ var CRLF = '\r\n';
       'Vary: *' + CRLF +
       'Content-Type: text/plain' + CRLF +
       CRLF);
-  var msg = new IncomingMessage();
+  var msg = new IncomingMessage()
+  // msg.on("Readable",function(data){
+  //   console.log(data)
+  // })
+  
   msg.execute(request)
+  // console.log("---------",msg.buffer)
+  // console.log("---------")
+  // msg.pipe(process.stdout)
+  // console.log(msg.read())
 })();
 
 
