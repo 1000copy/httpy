797f8721148a6a1054c3adb3d799c059af6bef04
1000copy <1000copy@gmail.com>
2015-09-04 12:17:54 +0800

    +send ok

--- udp/test.js
+++ udp/test.js
@@ -5,37 +5,82 @@ var HOST = '192.168.2.112';
 var dgram = require('dgram');
 //receiver 
 var server = dgram.createSocket('udp4');
-
 server.on('listening', function () {
     var address = server.address();
-    // server.setBroadcast(true);
-    // setInterval(broadcastNew, 3000);
+
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
+    sendipmsg()
+    // server.setBroadcast(true);
+    // var message = new Buffer("1:1441337488:air:book:18874369")
+    //  18874368  noop
+    // 
+    // 18874392 = 24 IPMSG_BR_ISGETLIST2
+	// server.send(message, 0, message.length, PORT, "192.168.2.255");
 });
-// var BROADCAST_ADDR ="255.255.255.255"
-// function broadcastNew() {
-//     var message = new Buffer("Broadcast message!");
-//     server.send(message, 0, message.length, PORT, BROADCAST_ADDR, function() {
-//         console.log("Sent '" + message + "'");
-//     });
-// }
+function sendipmsg(){
+	var ip = "192.168.2.115"
+	var message = new Buffer("1:1441337488:air:book:32:message")
+	server.send(message, 0, message.length, PORT, ip);	
+}
 
 server.on('message', function (message, remote) {
     console.log(remote.address + ':' + remote.port +' - ' + message);
-
+    // console.log(buf2msg(message))
 });
-
+function buf2msg(message){
+	var bsplit = require('buffer-split')
+    var snap = new Buffer(':')
+    var msg = {}
+    var list = bsplit(message,snap)
+    msg.version = list[0].toString() 
+    msg.no = list[1].toString() 
+    msg.name = list[2].toString() 
+    msg.group = list[3].toString() 
+    msg.commandraw = list[4].toString() 
+    msg.command = list[4].toString() & 0x000000ff 
+    if (list[5])
+    	msg.extension = list[5].toString() 
+	return msg
+}
 server.bind(PORT);
-// server.setBroadcast(true)
+/*
+1:1441335544:mobile:lcjiPhone.local:18874369:lcj�CiPhoneiPhone
+UN:mobile
+HN:lcjiPhone.local
+NN:lcj，iPhone
+GN:iPhone
 
-// // sender
 
-// // var dgram = require('dgram');
-// var message = new Buffer('My KungFu is Good!');
+#define IPMSG_NOOPERATION		0x00000000UL
 
-// var client = dgram.createSocket('udp4');
-// client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-//     if (err) throw err;
-//     console.log('UDP message sent to ' + HOST +':'+ PORT);
-//     client.close();
-// });
+#define IPMSG_BR_ENTRY			0x00000001UL
+#define IPMSG_BR_EXIT			0x00000002UL
+#define IPMSG_ANSENTRY			0x00000003UL
+#define IPMSG_BR_ABSENCE		0x00000004UL
+
+#define IPMSG_BR_ISGETLIST		0x00000010UL
+#define IPMSG_OKGETLIST			0x00000011UL
+#define IPMSG_GETLIST			0x00000012UL
+#define IPMSG_ANSLIST			0x00000013UL
+#define IPMSG_BR_ISGETLIST2		0x00000018UL
+
+#define IPMSG_SENDMSG			0x00000020UL
+#define IPMSG_RECVMSG			0x00000021UL
+#define IPMSG_READMSG			0x00000030UL
+#define IPMSG_DELMSG			0x00000031UL
+#define IPMSG_ANSREADMSG		0x00000032UL
+
+#define IPMSG_GETINFO			0x00000040UL
+#define IPMSG_SENDINFO			0x00000041UL
+
+#define IPMSG_GETABSENCEINFO	0x00000050UL
+#define IPMSG_SENDABSENCEINFO	0x00000051UL
+
+#define IPMSG_GETFILEDATA		0x00000060UL
+#define IPMSG_RELEASEFILES		0x00000061UL
+#define IPMSG_GETDIRFILES		0x00000062UL
+
+#define IPMSG_GETPUBKEY			0x00000072UL
+#define IPMSG_ANSPUBKEY			0x00000073UL
+
+*/
